23b8c6604cc2d67ec14520102c6d13c07e2d36ea
CesarAraujo <cesararaujo.perez@gmail.com>
2023-07-26 18:32:53 -0400

    Test1

--- src/app/firebase.js
+++ src/app/firebase.js
@@ -8,13 +8,13 @@ import { getFirestore, collection } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  //apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  //authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
   projectId: "notes-app-8ba2b",
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_MESSAGING_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MESSAGING_MEASUREMENT_ID,
+  //storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  //messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  //appId: process.env.REACT_APP_FIREBASE_MESSAGING_APP_ID,
+  //measurementId: process.env.REACT_APP_FIREBASE_MESSAGING_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
